9c0e64f1d1af83414b3ce6a3ef0e989144fc3e6c
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-04-14 18:34:33 -0300

    Adicionado o serviço que vai realizar a injeção das dependências e conexão com   o banco

--- CleanArchMvc.WebUI/Program.cs
+++ CleanArchMvc.WebUI/Program.cs
@@ -1,8 +1,11 @@
+using CleanArchMvc.Infra.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddInfrastructure(builder.Configuration);
 // Add services to the container.
-builder.Services.AddControllersWithViews();
 
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
