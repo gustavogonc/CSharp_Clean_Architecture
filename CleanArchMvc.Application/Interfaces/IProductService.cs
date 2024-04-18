30f49ddd3bfd026daf2daeccfd61d759cd9b2c0b
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-04-17 21:11:48 -0300

    Interface de IproductService criada

---
+++ CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Application.DTOs;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetProducts();
+        Task<ProductDTO> GetById(int? id);
+        Task<ProductDTO> GetBProductCategory(int? id);
+        Task Add(ProductDTO productDto);
+        Task Update(ProductDTO productDto);
+        Task Remove(int? id);
+    }
+}
