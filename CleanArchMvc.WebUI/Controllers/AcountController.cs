31e63cc0e70391a671b46f74c69dfed9dcc1d621
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-05-01 14:52:54 -0300

    Implementação dos métodos do login

---
+++ CleanArchMvc.WebUI/Controllers/AcountController.cs
@@ -0,0 +1,43 @@
+﻿using CleanArchMvc.Domain.Account;
+using CleanArchMvc.WebUI.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class AcountController : Controller
+    {
+        private readonly IAuthenticate _authentication;
+        public AcountController(IAuthenticate authentication)
+        {
+            _authentication = authentication;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await _authentication.Authenticate(model.Email, model.Password);
+
+            if (result)
+            {
+                if(string.IsNullOrEmpty(model.ReturnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return Redirect(model.ReturnUrl);
+            }else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt. (password must be strong).");
+                return View(model);
+            }
+        }
+    }
+}
