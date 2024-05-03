2eafe62468bee1231998cd667125d42097a06a75
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-05-02 22:06:15 -0300

    Endpoint para inclusão de uma nova categoria

--- CleanArchMvc.API/Controllers/CategoriesController.cs
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -27,7 +27,7 @@ namespace CleanArchMvc.API.Controllers
             return Ok(categories);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetCategory")]
         public async Task<ActionResult<CategoryDTO>> Get(int id)
         {
             var category = await _categoryService.GetById(id);
@@ -38,6 +38,19 @@ namespace CleanArchMvc.API.Controllers
             }
 
             return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDto)
+        {
+            if(categoryDto == null)
+            {
+                return BadRequest("Invalid data");
+            }
+
+            await _categoryService.Add(categoryDto);
+
+            return new CreatedAtRouteResult("GetCategory", new {id = categoryDto.Id}, categoryDto);
         }
     }
 }
