c956eede1f07af68dcab3197fd03ecb2a4e91a49
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-05-07 19:53:26 -0300

    Configuração dos serviços para adicionar a autorização no swagger

--- CleanArchMvc.API/Program.cs
+++ CleanArchMvc.API/Program.cs
@@ -3,13 +3,13 @@ using CleanArchMvc.Infra.IoC;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddInfrastructureJWT(builder.Configuration);
 builder.Services.AddInfrastructureAPI(builder.Configuration);
+builder.Services.AddInfrastructureSwagger();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
---
+++ CleanArchMvc.Infra.IoC/DependencyInjectionSwagger.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    public static class DependencyInjectionSwagger
+    {
+        public static IServiceCollection AddInfrastructureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "CleanArchMvc.API", Version = "V1" });
+
+                c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below. \r\n\r\nExample: \"Bearer 123456789abdefc\""
+                });
+
+                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                        new string []{}
+                    }
+                });
+            });
+
+            return services;
+
+        }
+    }
+}
