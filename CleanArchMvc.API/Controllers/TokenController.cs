b35bbcc92b1227d7ccfede4d751492671558af58
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-05-07 19:15:06 -0300

    gerando token e retornando no loginuser

--- CleanArchMvc.API/Controllers/TokenController.cs
+++ CleanArchMvc.API/Controllers/TokenController.cs
@@ -1,6 +1,11 @@
 ﻿using CleanArchMvc.API.Models;
 using CleanArchMvc.Domain.Account;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace CleanArchMvc.API.Controllers
 {
@@ -9,9 +14,11 @@ namespace CleanArchMvc.API.Controllers
     public class TokenController : ControllerBase
     {
         private readonly IAuthenticate _authentication;
-        public TokenController(IAuthenticate authentication)
+        private readonly IConfiguration _configuration;
+        public TokenController(IAuthenticate authentication, IConfiguration configuration)
         {
             _authentication = authentication;
+            _configuration = configuration;
         }
 
         [HttpPost("LoginUser")]
@@ -21,15 +28,46 @@ namespace CleanArchMvc.API.Controllers
 
             if (result)
             {
-                //return GenerateToken(userInfo);
-                return Ok($"Yser {userInfo.Email} login successfully");
+                return GenerateToken(userInfo);
+                //return Ok($"Yser {userInfo.Email} login successfully");
             }
             else
             {
                 ModelState.AddModelError(string.Empty, "Invalid Login attempt.");
                 return BadRequest(ModelState);
             }
-        } 
+        }
+        
+        private UserToken GenerateToken(Login userInfo)
+        {
+            var claims = new[]
+            {
+                new Claim("email", userInfo.Email),
+                new Claim("meuvalor", "oque voce quiser"),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var privateKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]!));
+
+            var credentials = new SigningCredentials(privateKey, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddMinutes(10);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: expiration,
+                signingCredentials: credentials
+                );
+
+
+            return new UserToken(){
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
+
+        }
 
     }
 }
